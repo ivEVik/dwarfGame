3109370efd1bee551c2c8b5470f306a3555d14c4
Siigiil <sergantcom248@gmail.com>
2022-04-06 12:42:59 +0500

    Fixed tabulation.

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,11 @@ using System.Windows.Forms;
 
 namespace dwarfGame
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-    }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -6,18 +6,15 @@ using System.Windows.Forms;
 
 namespace dwarfGame
 {
-    static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
 }
